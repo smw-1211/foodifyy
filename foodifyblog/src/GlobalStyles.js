7ac3bbeb7ff30d6fcbf4e777a37b7d9c6a9fae8d
smw-1211 <wattamwarsoham2000@gmail.com>
2023-10-10 16:57:02 +0530

    Global styles sheet updated

--- foodifyblog/src/GlobalStyles.js
+++ foodifyblog/src/GlobalStyles.js
@@ -56,3 +56,79 @@ width: 100%;
     padding: 0 30px;
 }
 `;
+
+export const Button = styled.button`
+border-radius: ${({bigRadius}) => bigRadius ? '30px': '20px'};
+background-color: ${({primary}) => primary? '#E38B06': '#000'};
+color: ${({primary}) => primary ? '#000': '#fff'};
+padding: ${({big}) => big? '18px 30px' : '10px 28px'};
+font-size: ${({bigFont}) => bigFont? '20px': '18px'};
+outline: none;
+cursor: pointer;
+border: none;
+transition: all .5s ease;
+
+&:hover{
+    background-color: ${({primary}) => primary? '#fff': '#E38B06'};  
+    transform: translateY(-.5rem) scale(1.02);
+    color: #000;
+}
+&:active{
+    transform: translateY(.5rem);
+}
+
+@media only screen and (max-width:1000px) {
+    /* width: 100%; */
+    padding: ${({big}) => big? '18px 30px' : '10px 20px'};
+}
+@media only screen and (max-width:375px) {
+    padding: ${({big}) => big? '12px 20px' : '10px 20px'};
+    font-size: ${({bigFont}) => bigFont? '16px': '18px'};
+}
+
+
+`;
+
+export const OutlineButton = styled.button`
+border-radius: ${({bigRadius})=> bigRadius? '40px' : '30px'};
+border: 2px solid #333;
+color: #333;
+outline: none;
+padding: ${({big}) => big? '15px 60px' : '13px 55px'};
+font-size: ${({fontBig}) => fontBig? '22px':'18px'};
+transition: all .5s ease;
+background-color: #fefefe;
+
+
+&:hover {
+    background-color: #333;
+    color: #fff;
+    border: none;
+    transform: translateY(-.5rem) scale(1.02);
+}
+&:active{
+    transform: translateY(.5rem);
+}
+
+
+@media only screen and (max-width: 1200px) {
+    border-radius: ${({bigRadius})=> bigRadius? '20px' : '18px'};
+    padding: ${({big}) => big? '9px 30px' : '8px 28px'};
+    font-size: ${({fontBig}) => fontBig? '18px':'16px'};
+}
+
+@media only screen and (max-width:780px) {
+    border: none;
+    color: #E38B06;
+    padding: 1rem 2rem;
+    background: none;
+    transition: all .4s ease;
+
+    &:hover {
+        border-bottom: 1px solid #E38B06; 
+        background: none; 
+        border-radius: 0;
+        color: #E38B06;
+    }
+}
+`;
