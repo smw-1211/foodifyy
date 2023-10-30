1117435dce6800b02290f212ea980d98d74ac7e6
smw-1211 <wattamwarsoham2000@gmail.com>
2023-10-30 21:56:05 +0530

    Updated the App.js boilerplate code with project code

--- foodifyblog/src/App.js
+++ foodifyblog/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {Hero, Footer} from './components';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import GlobalStyles from './Globalstyles';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyles/>
+      <Hero/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
