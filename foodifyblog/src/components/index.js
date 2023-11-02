a7dfb9830032ac8df8c2582365d2476f4150c613
smw-1211 <wattamwarsoham2000@gmail.com>
2023-11-02 22:28:31 +0530

    Updated the index.js file

--- foodifyblog/src/components/index.js
+++ foodifyblog/src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Welcome} from './Welcome/Welcome';
 export {default as Meals} from './Meals/Meals';
-export {default as Footer} from './/Footer/Footer';
+export {default as Footer} from './Footer/Footer';
+export {default as Navbar} from './Navbar/Navbar'
