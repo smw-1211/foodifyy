c484b19d68e8963461f7f547520ed16988b98b78
smw-1211 <wattamwarsoham2000@gmail.com>
2023-11-06 22:40:59 +0530

    Updated the stylesheet of works component

--- foodifyblog/src/components/Works/Works.styles.js
+++ foodifyblog/src/components/Works/Works.styles.js
@@ -35,3 +35,89 @@ margin-top: 5rem;
     flex-direction: column;
 }
 `;
+
+export const WorksCard = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+text-align: center;
+width: 30rem;
+height: 30rem;
+background-color: #fff;
+box-shadow: 0px 5px 40px rgba(0, 0, 0, 0.19);
+border-radius: 20px;
+transition: all .5s ease;
+
+&:not(:last-child) {
+    margin-right: 5rem;
+
+    @media only screen and (min-width: 1300px){
+        margin-right: 10rem;
+    }
+
+    @media only screen and (min-width: 1500px){
+        margin-right: 20rem;
+    }
+    @media only screen and (max-width:900px){
+        margin-bottom: 10rem;
+        margin-right: 0;
+    }
+    @media only screen and (max-width: 800px){
+        box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.12);
+    }
+}
+
+@media only screen and (max-width:900px){
+        width: 50rem;
+}
+@media only screen and (max-width:500px){
+        width: 30rem;
+}
+@media only screen and (min-width:1890px){
+        width: 40rem;
+}
+&:hover {
+    box-shadow:  0px 10px 80px rgba(0, 0, 0, 0.21);
+    transform: scale(1.05);
+    background-color: #E38B06;
+    color: #fff;
+    
+}
+`;
+
+export const WorksIconContainer = styled.div`
+width: 9rem;
+height: 9rem;
+border-radius: 50%;
+border: 2px solid #333;
+transition: all .3s ease-out;
+margin-top: 2rem;
+display: flex;
+justify-content: center;
+align-items: center;
+
+`;
+export const WorksIcon1 = styled(BiCheck)`
+color: #333;
+font-size: 8rem;
+`;
+
+export const WorksIcon2 = styled(BiDetail)`
+color: #333;
+font-size: 6rem;
+`;
+export const WorksIcon3 = styled(BiDish)`
+color: #333;
+font-size: 6rem;
+`;
+
+export const WorksCardTitle = styled.h3`
+font-size: 2.4rem;
+font-weight: bold;
+padding-top: 1rem;
+`;
+
+export const WorksCardText = styled.p`
+font-size: 1.8rem;
+padding: 1rem 2rem;
+`;
