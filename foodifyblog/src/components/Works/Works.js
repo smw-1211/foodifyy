0a9b3fa088fdc64350131f5364d21401e14789bd
smw-1211 <wattamwarsoham2000@gmail.com>
2023-11-07 13:13:25 +0530

    Started making the works component

---
+++ foodifyblog/src/components/Works/Works.js
@@ -0,0 +1,22 @@
+import {
+    WorksContainer,
+    WorksContent,
+    WorksTitle,
+    WorksCardContent,
+} from './works.styles';
+const Works = () => {
+    return (
+        <div>
+            <WorksContent>
+                <WorksContainer>
+                    <WorksTitle> How it works</WorksTitle>
+                    <WorksCardContent>
+                    </WorksCardContent>
+                </WorksContainer>
+            </WorksContent>
+            
+        </div>
+    )
+}
+
+export default Works;
