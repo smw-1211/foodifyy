249c93bca2ad52cb4474b86cb6935567ec4cdc72
smw-1211 <wattamwarsoham2000@gmail.com>
2023-11-14 12:16:04 +0530

    Started off with a new component - Recipes

--- foodifyblog/src/components/index.js
+++ foodifyblog/src/components/index.js
@@ -4,3 +4,4 @@ export {default as Footer} from './Footer/Footer';
 export {default as Navbar} from './Navbar/Navbar'
 export {default as Hero} from './Hero/Hero';
 export {default as Works} from './Works/Works';
+export {default as Recipes} from './Recipes/Recipes';
