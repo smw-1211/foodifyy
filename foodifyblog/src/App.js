fbef6356c466177eaedb88e9f23d474be9239621
smw-1211 <wattamwarsoham2000@gmail.com>
2023-12-08 19:20:06 +0530

    Added Recipe and Order Page to the app function

--- foodifyblog/src/App.js
+++ foodifyblog/src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import {Hero, Footer} from './components';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Recipe from './pages/Recipe/Recipe';
+import Order from './pages/Order/Order';
 import GlobalStyles from './Globalstyles';
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
+        <Route exact path="/recipe" component={Recipe}/>
+        <Route exact path="/order-now" component={Order}/>
       </Switch>
       <Footer/>
     </Router>
